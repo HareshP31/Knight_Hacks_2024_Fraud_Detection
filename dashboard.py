f27d6c3cbe9d5491e2ff90158587fdae99fa413a
Nickie <nickiesethi@gmail.com>
2024-10-05 23:22:46 -0400

    hi

--- dashboard.py
+++ dashboard.py
@@ -78,12 +78,6 @@ if user_csv is not None:
     if not os.path.exists(temp_dir):
         os.makedirs(temp_dir)
 
-    # Streamlit app title
-    st.title('Financial Fraud Detector!')
-
-    # Logic for uploading files, processing data, etc.
-    user_csv = st.file_uploader("Upload your CSV file here", type="csv")
-
     if user_csv is not None:
         file_path = os.path.join(temp_dir, user_csv.name)  # Save to the temp directory
         with open(file_path, "wb") as f:
